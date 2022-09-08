f92a00ebb0026d9fbb3481d03b6a3ce009cf5f42
Lila <dalila.analistadesistema@gmail.com>
2022-09-08 19:01:25 -0300

    cOMMIT

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,1 +1,15 @@
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+
+/*
 console.log('Tudo Bem')
+console.log('Tudo Bem')
+console.log('Tudo Bem')
+console.log('Tudo Bem')
+console.log('Tudo Bem')
+console.log('Tudo Bem')
+*/
